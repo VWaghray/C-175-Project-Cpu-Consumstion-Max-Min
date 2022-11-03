27e37396a6e0057bea2e09b2c2ffd1daa0525921
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-11-03 16:09:31 -0400

    Add files via upload

---
+++ C175 Project Cpu Consumtion -- Max and Min.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import psutil as p
+app_name_dict = {}
+count = 0
+
+for process in p.process_iter():
+    count = count+1
+    if count <= 6:
+        name = process.name()
+        cpu_usage = p.cpu_percent()
+        print('Name: ', name,'-- Cpu Usage: ', cpu_usage)
+        app_name_dict.update({name:cpu_usage})
+        
+        
+keymax = max(app_name_dict, key = app_name_dict.get)
+print(keymax)
+keymin = min(app_name_dict, key = app_name_dict.get)
+print(keymin)
+name_list = [keymax,keymin]
+print(name_list)
+
+app=app_name_dict.values()
+max_app = max(app)
+print("Maximum", max_app)
+min_app = min(app)
+print("Minimum", min_app)
+max_min_list=[max_app,min_app]
+print(max_min_list)
+
+plt.figure(figsize=(15,8))
+plt.xlabel("Min and Max Cpu Consumption")
+plt.ylabel("Usage")
+plt.bar(name_list,max_min_list,width=0.6,color=("crimson","blue"))
+plt.show()
